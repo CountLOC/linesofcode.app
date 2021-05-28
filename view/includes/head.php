c527fa14b3d0808f9304726be677bdb29700f8e1
Adam McGurk <amcgurk@bamboohr.com>
2021-05-28 14:29:45 -0600

    Working on the base stuff

---
+++ view/includes/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-us">
+<head>
+
+	<meta charset="utf-8">
+	<title>Count LOC</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+	<link href="/style/style.css" rel="stylesheet">
+	<link rel="icon" type="image/png" href="/favicon.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
