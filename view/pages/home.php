b142b4d3830dca63614ee604f855aa1c1163b52a
Adam McGurk <amcgurk@bamboohr.com>
2021-05-28 15:31:04 -0600

    Some homepage content

--- view/pages/home.php
+++ view/pages/home.php
@@ -1,9 +1,19 @@
-<main class="max-width">
+<main class="max-width" id="homePageMain">
 	
 	<section class="content">
 		
-		<div class="words"></div>
-		<div class="assets"></div>
+		<div class="words">
+			<h1>Instantly get the number of lines of code written in <i>any</i> GitHub repository.</h1>
+			<p>Input your GitHub URL and get the amount of lines of code written, broken down by language. It's that simple.</p>
+			<div class="cta-wrap">
+				<a href="/app" title="Go to the app" class="button red">Try it out now</a>
+				<a href="/data-handling" title="Our data handling promise to you" class="button blue">Our data handling promise</a>
+			</div>
+		</div>
+
+		<div class="assets">
+			<img src="/assets/illustrations/homepage-code.svg" alt="A man looking at a visulatization of code" title="linesofcode.app">
+		</div>
 
 	</section>
 
