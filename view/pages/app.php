66c6eb4cb1dabafc5d6ee6398e8b7fcb3ea8e56f
Adam McGurk <amcgurk@bamboohr.com>
2021-05-28 17:00:41 -0600

    Starting this app checking ish off STRONG

---
+++ view/pages/app.php
@@ -0,0 +1,20 @@
+<main class="max-width" id="homePageMain">
+	
+	<section class="content" data-page="1">
+		
+		<div class="words">
+			<h1>Step 1</h1>
+			<p>Input your Github URL into the box below!</p>
+			<div class="cta-wrap">
+				<a href="/app" title="Go to the app" class="button red">Try it now</a>
+				<a href="/data-handling" title="Our data handling promise to you" class="button blue">Our data handling promise</a>
+			</div>
+		</div>
+
+		<div class="assets">
+			<img src="/assets/illustrations/vcs.svg" alt="A man looking at a visulatization of code" title="linesofcode.app">
+		</div>
+
+	</section>
+
+</main>
