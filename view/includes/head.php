d6b78c53eaf52357ca8e0aca9ea3b833b22cd81b
Adam McGurk <amcgurk@bamboohr.com>
2021-05-28 15:09:06 -0600

    Hopefully fixed the font situation

--- view/includes/head.php
+++ view/includes/head.php
@@ -5,10 +5,10 @@
 	<meta charset="utf-8">
 	<title>LinesOfCode</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Montserrat:wght@300;400;500;600&display=swap">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:wght@300;400;500;600&display=swap" media="print" onload="this.media='all'">
+	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" media="print" onload="this.media='all'">
 	<noscript>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:wght@300;400;500;600&display=swap">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap">
 	</noscript>
 	<link href="/css/style.css" rel="stylesheet">
 	<link rel="icon" type="image/png" href="/favicon.png">
