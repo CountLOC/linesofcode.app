ba9c2fe378d93f80ed5e1696b53fe73c28375b67
Adam McGurk <amcgurk@bamboohr.com>
2021-05-29 11:00:34 -0600

    progress bar is done!!!

--- view/includes/header-content.php
+++ view/includes/header-content.php
@@ -1,10 +1,13 @@
 <div id="sticky-footer-wrap">
 
 <header class="max-width">
+
 	<a href="/" title="Go back to the home page"><img src="/assets/logo.svg" title="Logo for linesofcode.app" alt="The letters spelling out CountLOC with a logo of three uneven colored lines"></a>
+
 	<?php if (IS_APP): ?>
-	<div id="progressBar"><span class="middle"></span></div>
+		<div id="progressBar" data-step="1"><span class="middle"></span></div>
 	<?php else: ?>
-	<a href="/app" title="Go to the app" class="app-cta">Launch App</a>
+		<a href="/app" title="Go to the app" class="app-cta">Launch App</a>
 	<?php endif; ?>
+
 </header>
