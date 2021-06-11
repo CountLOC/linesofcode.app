68fdbe339adaf0dd3d2d247fc31283d744126297
Adam McGurk <amcgurk@bamboohr.com>
2021-06-11 14:15:38 -0600

    Added this new page helper to move things along when needed

---
+++ scripts/View/App/Helpers/Page/PageHelper.ts
@@ -0,0 +1,33 @@
+class PageHelper {
+
+	public static moveForwardOne(): void {
+		const currentShownPage:HTMLElement = PageHelper.getCurrentShownPage();
+		const desiredPageNumber = parseInt(currentShownPage.getAttribute("data-page")) + 1;
+		PageHelper.moveToPage(desiredPageNumber);
+	}
+
+	public static moveBackwardOne(): void {
+		const currentShownPage:HTMLElement = PageHelper.getCurrentShownPage();
+		const desiredPageNumber = parseInt(currentShownPage.getAttribute("data-page")) - 1;
+		PageHelper.moveToPage(desiredPageNumber);
+	}
+
+	public static moveToPage(pageNumber:number): void {
+		const pages:NodeListOf<HTMLElement> = document.querySelectorAll("[data-page]");
+		const desiredPage:HTMLElement = document.querySelector(`[data-page=${pageNumber}]`);
+		if (desiredPage == null) {
+			throw "The page with number of " + pageNumber + " does not exist";
+		}
+		PageHelper.closeAllPages(pages);
+		desiredPage.removeAttribute("hidden");
+	}
+
+	private static closeAllPages(pages:NodeListOf<HTMLElement>): void {
+		pages.forEach(page => page.setAttribute("hidden", "true"));
+	}
+
+	private static getCurrentShownPage(): HTMLElement {
+		return document.querySelector("[data-page]:not([hidden])");
+	}
+
+}
