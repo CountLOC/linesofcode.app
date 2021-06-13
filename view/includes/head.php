f6dcf3007a81ccf07b76f1fb82bb19c411ff1ac4
Adam McGurk <amcgurk@bamboohr.com>
2021-06-12 21:36:02 -0600

    Adding the base API url to the http document

--- view/includes/head.php
+++ view/includes/head.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-us">
 <head>
-
 	<meta charset="utf-8">
 	<title>LinesOfCode</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,7 +12,7 @@
 	<link href="/css/style.css" rel="stylesheet">
 	<link rel="icon" type="image/png" href="/favicon.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<meta name="api-url" content="<?=$apiBaseUrl?>">
 </head>
 
 <body>
