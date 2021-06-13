673193801e84a730c9b33c4f7a84df12b064fb5f
Adam McGurk <amcgurk@bamboohr.com>
2021-06-12 21:29:47 -0600

    Added the method to get the count...let's GO

--- scripts/Model/Http/ApiHelper.ts
+++ scripts/Model/Http/ApiHelper.ts
@@ -1,8 +1,22 @@
+interface LanguageDataObject {
+	total:number;
+	language:string;
+}
+
+interface CountReturn {
+	total:number;
+	languages:Array<LanguageDataObject>;
+} 
+
 class ApiHelper extends HttpHelper {
 
 	constructor() {
 		super();
 		this.apiBase = "";
+	}
+
+	public async getNoAuthCountReturn(repoUrl:string): Promise<CountReturn> {
+		return await this.sendRequest(`${this.apiBase}/count?repo_url=${repoUrl}`);
 	}
 
 }
--- scripts/Model/Http/HttpHelper.ts
+++ scripts/Model/Http/HttpHelper.ts
@@ -7,7 +7,7 @@ abstract class HttpHelper {
 
 	protected apiBase:string;
 	
-	protected async sendRequest(fullPath:string, method:string = "GET", headers:Array<HeaderInterface>): Promise<any> {
+	protected async sendRequest(fullPath:string, method:string = "GET", headers:Array<HeaderInterface> = []): Promise<any> {
 		const fetchHeaders = new Headers();
 		headers.forEach(header => fetchHeaders.append(header.name, header.value));
 		const fetchReturn:Response = await fetch(this.apiBase + fullPath, {
