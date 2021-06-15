757a3904a0c9c79933803a06658a738f4b83ee2d
Adam McGurk <amcgurk@bamboohr.com>
2021-06-15 14:55:59 -0600

    Added the first cypress test

---
+++ cypress/integration/basic-tests.js
@@ -0,0 +1,6 @@
+describe("Basic one-off tests", () => {
+	it("Tries to go to the oauth page without a code", () => {
+		cy.visit("/oauth-consume");
+		cy.location.pathname.should("equal", "/");
+	});
+});
