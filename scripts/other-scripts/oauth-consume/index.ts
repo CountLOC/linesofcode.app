40a5332fbe30cd04e991630d97cc5d647e38feb0
Adam McGurk <amcgurk@bamboohr.com>
2021-06-15 14:53:47 -0600

    First crack at this oauth stuff

---
+++ scripts/other-scripts/oauth-consume/index.ts
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (ev:Event) => {
+	const searchParams:URLSearchParams = new URLSearchParams(window.location.search);
+	const oauthCode:string|null = searchParams.get("code");
+	if (oauthCode == null) {
+		window.location.href = "/";
+	}
+	sessionStorage.setItem("code", oauthCode);
+	console.log("nav away");
+}, false);
---
+++ view/pages/oauth-consume.php
@@ -0,0 +1,1 @@
+<script src="/js/oauth-consume.js"></script>
