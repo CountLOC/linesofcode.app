5c5fb009144eda8e19c04825ffd3af442a1a325f
Adam McGurk <amcgurk@bamboohr.com>
2021-06-16 13:49:48 -0600

    Now the image should be good@

--- view/pages/oauth-consume.php
+++ view/pages/oauth-consume.php
@@ -1,4 +1,4 @@
 <main class="max-width" id="homePageMain">
-	<img src="/assets/illustrations/oauth.svg" alt="" title="Waiting" style="margin: 0 auto; opacity: .7; max-width: 500px;">
+	<img src="/assets/illustrations/oauth.svg" alt="" title="Waiting" style="margin: 0 auto; opacity: .7; max-width: 500px; width: 95%;">
 </main>
 <script src="/js/oauth-consume.js"></script>
